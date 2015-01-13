90456ad4d5f7145092a0622e75e58af14dda35e1
zRouth <zrouthier@gmail.com>
2015-01-13 13:21:24 -0700

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
 
   root "welcome#index"
+
+  resources :sessions
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
