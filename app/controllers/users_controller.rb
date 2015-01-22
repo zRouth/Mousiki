2903863d2dc71e9a8bd40a57519b6023bd34af82
zRouth <zrouthier@gmail.com>
2015-01-22 09:12:47 -0700

    uniq artist name from json hash on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @client = client(@user)
+    @artists = artist_name.uniq
   end
 
   private
@@ -35,6 +36,10 @@ class UsersController < ApplicationController
     :write_timeout => 10,   # set longer write_timeout, default is 10 seconds
     :persistent    => false # when true, make multiple requests calls using a single persistent connection. Use +close_connection+ method on the client to manually clean up sockets
   }
+  end
+
+  def artist_name
+    artists = @client.me_tracks['items'].map { |item| item['track']['artists'].first['name']}
   end
 
 end
