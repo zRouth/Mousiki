5959606071364e3ef45a74f76900d618a00c1444
David Bayendor <dbayendor@gmail.com>
2015-01-28 11:19:35 -0700

    Update to use ENV variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, '87623219a8154463ba31071932d5c101', '1c747e59974c4b9da1d851259c05788e',
+  provider :spotify, ENV['SPOTIFY_APP_ID'], ENV['SPOTIFY_APP_SECRET'],
             scope: 'playlist-read-private user-read-private user-read-email user-library-read user-library-modify user-follow-read'
 end
