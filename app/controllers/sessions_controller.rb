320e0f4c9860b6c4271f23184e6fc375aff386a7
David Bayendor <dbayendor@gmail.com>
2015-01-28 15:09:51 -0700

    Add current user method.
    * Closes #6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    redirect_to '/auth/spotify'
   end
 
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    # self.current_user = @user
+    reset_session
+    session[:user_id] = @user.id
     redirect_to user_path(@user)
    end
+
+  def destroy
+    reset_session
+    redirect_to root_url
+  end
 
   protected
 
