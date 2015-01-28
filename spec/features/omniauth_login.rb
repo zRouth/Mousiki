296e0de52f00f8dfbee196adcfe7f8dd348992ed
David Bayendor <dbayendor@gmail.com>
2015-01-28 16:13:10 -0700

    Add tests for oauth in sessions controller
    * Closes #10

---
+++ spec/features/omniauth_login.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+describe 'omniauth login', type: :feature do
+  before do
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:spotify]
+  end
+end
