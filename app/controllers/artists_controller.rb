572a9fe64653488a7a2b733c82cf2720acb8290a
zRouth <zrouthier@gmail.com>
2015-05-13 14:10:22 -0600

    extracted artist controller logic into services

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,55 +1,17 @@
 class ArtistsController < ApplicationController
   def show
-    @artist_id = get_artist_id
-    @calendar = get_artist_calendar
-  end
-
-  # pass the artist name to params
-  # get artist id
-  # use artist name to construct search query
-  # get the artist id returned in the JSON query
-  # get artist calendar
-  # use artist id to construct a new calandar query for that artist
-  # parse JSON
-  # put calendar on show page
-
-  private
-
-  def get_artist_id
-    response = Faraday.get(artist_request)
-    body = JSON.parse(response.body)
-    if body['resultsPage']['results']['artist']
-      body['resultsPage']['results']['artist'].first['id']
+    begin
+      @calendar = SongkickEvents.new(params[:name]).artist_calendar
+    rescue SongkickEvents::NoEventsFound => e
+      redirect_to user_path(current_user), notice: e.msg
     end
   end
 
-  def get_artist_calendar
-    response = Faraday.get(calendar_request)
-    body = JSON.parse(response.body)
-
-    if body['resultsPage']['results'] == {} || !body['resultsPage']['results']
-      redirect_to user_path(current_user), notice: 'No upcoming shows for that artist'
-    else
-      body['resultsPage']['results']['event'].select do |event|
-        event['performance'].first['artist']['id'] == @artist_id
-      end
-    end
-  end
-
-  def songkick
-    'http://api.songkick.com/api/3.0/'
-  end
-
-  def songkick_api
-    'apikey=Pk55qLCyIJJesrG6'
-  end
-
-  def artist_request
-    songkick + "search/artists.json?query=#{params[:name]}&" + songkick_api
-  end
-
-  def calendar_request
-    songkick + "artists/#{@artist_id}/calendar.json?" + songkick_api
-  end
-
+  # def create
+  #   if CreateUser.new(params)
+  #     render :success
+  #   else
+  #     render :failure
+  #   end
+  # end
 end
